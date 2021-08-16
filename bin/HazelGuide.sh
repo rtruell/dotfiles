ebb464725a0fe1fff540b5dd6f7ff50ab2c49449
Rick Truell <rtruell-github@rtruell.ca>
2021-08-16 00:22:26 -0600

    Partially corrected script file broken by new dotfiles setup.

--- bin/HazelGuide.sh
+++ bin/HazelGuide.sh
@@ -3,4 +3,14 @@
 StartDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "${StartDir}"
 
-grep -v '^    $' "$1" | grep -v '^$' | sed -E -e 's/TV Guide for ([[:alnum:]]+) /\1, /' -e 's/([a|p]).m.([-|\&])/\u\1M\2/g' -e 's/ ([A|P]M[-|\&])/\1/g' -e 's/ends //' -e 's/([[:digit:]]) [[:alnum:]]+\&/\1\&/' -e 's/\&nbsp;\&nbsp;\&nbsp;<br>/ ()\&nbsp;\&nbsp;\&nbsp;<br>/' #| pbcopy
+#grep -v '^    $' "$1" | grep -v '^$' | sed -e 's/TV Guide for \([[:alnum:]]\+\) /\1\, /' -e 's/\(\[a|p]\).m.\(\[-|\&]\)/\u\1M\2/g' -e 's/ \(\[A|P]M\[-|\&]\)/\1/g' -e 's/ends //' -e 's/\([[:digit:]]\) [[:alnum:]]\+\&/\1\&/' -e 's/\&nbsp;\&nbsp;\&nbsp;<br>/ ()\&nbsp;\&nbsp;\&nbsp;<br>/' | pbcopy
+
+#grep -v '^    $' "$1" | grep -v '^$' | sed -E -e 's/TV Guide for ([[:alnum:]]+) /\1, /' -e 's/ ([a|p]).m.(-)/\1m\2/' -e 's/ ([a|p]).m.(&)/\1m\2/' -e 's/ ([A|P]M[-|\&])/\1/g' -e 's/ends //' -e 's/([[:digit:]]) [[:alnum:]]+\&/\1\&/' -e 's/\&nbsp;\&nbsp;\&nbsp;<br>/ ()\&nbsp;\&nbsp;\&nbsp;<br>/' | pbcopy
+
+grep -v '^    $' "$1" | grep -v '^$' | sed -E -e 's/TV Guide for ([[:alnum:]]+) /\1, /' -e 's/ ([a|p]).m.(-)/\1m\2/' -e 's/ ([a|p]).m.(&)/\1m\2/' -e 's/ends //' -e 's/([[:digit:]]) [[:alnum:]]+\&/\1\&/' -e 's/\&nbsp;\&nbsp;\&nbsp;<br>/ ()\&nbsp;\&nbsp;\&nbsp;<br>/' | pbcopy
+
+#grep -v '^    $' "$1" | grep -v '^$' | \
+#sed -E 's/TV Guide for ([[:alnum:]]+) /\1, /' | \
+#sed -E 's/([a|p]).m.([-|\&])/\u\1M\2/g' ] | \
+#sed -E 's/ ([A|P]M[-|\&])/\1/g' | \
+#sed -E -e 's/ends //' -e 's/([[:digit:]]) [[:alnum:]]+\&/\1\&/' -e 's/\&nbsp;\&nbsp;\&nbsp;<br>/ ()\&nbsp;\&nbsp;\&nbsp;<br>/' | pbcopy
