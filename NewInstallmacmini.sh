73c9e4c98f8343a51c1e8d37731ac57c33e89d99
Rick Truell <rtruell-github@rtruell.ca>
2021-10-28 21:30:50 -0600

    Modified so that it's appropriate for my setup.

--- NewInstallmacmini.sh
+++ NewInstallmacmini.sh
@@ -2,6 +2,17 @@
 StartDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "${StartDir}"
 
+# some of my functions are needed to set things up, so load them
+source ./.functions/execute_command.function
+source ./.functions/findcommand.function
+source ./.functions/print_error.function
+source ./.functions/print_result.function
+source ./.functions/print_success.function
+source ./.functions/print_warn.function
+source ./.functions/symlink_array_files.function
+source ./.functions/symlink_single_file.function
+
+# ANSI sequences for colours to be used in the banner
 declare -a colours=(
   "\033[0;30m"
   "\033[1;30m"
@@ -15,6 +26,7 @@ declare -a colours=(
   "\033[0;37m"
 )
 
+# a banner display in the style as shown in the movie "Matrix"
 for z in {1..40}; do
   for i in {1..16}; do
     r="$(($RANDOM % 2))"
@@ -32,6 +44,7 @@ for z in {1..40}; do
   v="";
 done
 
+# emphasize that this is a new install :-)
 printf "\033[0m\n"
 printf " #    # ###### #    #       # #    #  ####  #####   ##   #      #        ### ###\n"
 printf " ##   # #      #    #       # ##   # #        #    #  #  #      #        ### ###\n"
@@ -42,319 +55,149 @@ printf " #    # ###### #    #       # #    #  ####    #   #    # ###### ######  
 printf "\n"
 unset colours z i r v
 
-# Check if /usr/bin/sudo and /bin/bash exist. if not, try to find them and suggest a symlink
+# Check if /usr/bin/sudo and /bin/bash exist. if not, try to find them and
+# suggest a symlink, then exit the script
 if [[ ! -f /usr/bin/sudo ]]; then
   if findcommand sudo &>/dev/null; then
-    printf -- '%s\n' "/usr/bin/sudo not found.  Please run 'sudo ln -s $(findcommand sudo) /usr/bin/sudo'"
+    message="/usr/bin/sudo not found.  Please run 'sudo ln -s $(findcommand sudo) /usr/bin/sudo'"
   else
-    printf -- '%s\n' "/usr/bin/sudo not found, and I couldn't find 'sudo' in '$PATH'"
+    message="/usr/bin/sudo not found, and I couldn't find 'sudo' in '\$PATH'"
   fi
-  exit
+  print_result 1 "${message}" "true"
 fi
+print_result 0 'Found sudo'
 if [[ ! -f /bin/bash ]]; then
   if findcommand bash &>/dev/null; then
-    printf -- '%s\n' "/bin/bash not found.  Please run 'sudo ln -s $(findcommand bash) /bin/bash'"
+    message="/bin/bash not found.  Please run 'sudo ln -s $(findcommand bash) /bin/bash'"
   else
-    printf -- '%s\n' "/bin/bash not found, and I couldn't find 'bash' in '$PATH'"
+    message="/bin/bash not found, and I couldn't find 'bash' in '\$PATH'"
   fi
-  exit
+  print_result 1 "${message}" "true"
+fi
+print_result 0 'Found bash'
+
+# symlink the dotfiles into ${HOME}
+source ./symlink.sh
+print_result $? 'Symlinked dotfiles'
+
+# Copy over the files and directories that are needed but shouldn't be in a
+# public repository
+declare -a filesdirs=(
+  ".credentials"
+  ".gitconfig.local"
+  ".ssh"
+)
+file=""
+i=""
+retcode=""
+currdir=$(PWD)
+# mount the NAS's data directory.  this will prompt for the user/password, then
+# try to create '/Volumes/data' and mount the NAS's data directory there
+osascript -e "mount volume \"smb://fileserver/data\"" 1>/dev/null 2>&1
+retcode=$?
+if [[ "${retcode}" == 0 ]]; then  # if the NAS was mounted
+  print_result ${retcode} 'Mounted NAS'
+  cd /Volumes/data/OSInstallFiles  # change to the directory containing the files/directories to be copied
+  print_result $? 'Changed to the directory containing the sensitive files/directories to be copied'
+  for i in ${filesdirs[@]}; do  # loop through the array of files and directories
+    if [[ -d "${i}" ]]; then  # if it's a directory
+      cp -ra "${i}" ~  # copy it and all its files
+      print_result $? 'Copied directory ${i}'
+      chmod 700 ~/"${i}"  # set the permissions on the directory itself to read/write/execute for the owner and nothing for others
+      print_result $? 'Set permissions for the ${i} directory'
+      chmod 600 ~/"${i}"/*  # set the permissions on the files in the directory to read/write for the owner and nothing for others
+      print_result $? 'Set permissions for the files in the ${i} directory'
+    else  # otherwise it's a file
+      cp -a "${i}" ~  # copy it
+      print_result $? 'Copied ${i}'
+      chmod 600 ~/"${i}"  # set its permissions to read/write for the owner and nothing for others
+      print_result $? 'Set permissions for ${i}'
+    fi
+  done
+  diskutil unmount /Volumes/data 1>/dev/null 2>&1  # unmount the NAS.  this also automatically deletes '/Volumes/data'
+  print_result $? 'Unmounted NAS'
+  cd "${currdir}"  # change back to where we were
+else
+  print_result ${retcode} 'Mounting NAS failed...sensitive files/directories must be copied manually'
 fi
 
-# copy paste this file in bit by bit.
-# don't run it.
-  echo "do not run this script in one go. hit ctrl-c NOW"
-  read -n 1
-
-
-##############################################################################################################
-###  backup old machine's key items
-
-mkdir -p ~/migration/home/
-mkdir -p ~/migration/Library/"Application Support"/
-mkdir -p ~/migration/Library/Preferences/
-mkdir -p ~/migration/Library/Application Support/
-mkdir -p ~/migration/rootLibrary/Preferences/SystemConfiguration/
-
-cd ~/migration
-
-# what is worth reinstalling?
-brew leaves              > brew-list.txt    # all top-level brew installs
-brew cask list           > cask-list.txt
-npm list -g --depth=0    > npm-g-list.txt
-yarn global list --depth=0 > yarn-g-list.txt
-
-# then compare brew-list to what's in `brew.sh`
-#   comm <(sort brew-list.txt) <(sort brew.sh-cleaned-up)
-
-# backup some dotfiles likely not under source control
-cp -Rp \
-    ~/.bash_history \
-    ~/.extra ~/.extra.fish \
-    ~/.gitconfig.local \
-    ~/.gnupg \
-    ~/.nano \
-    ~/.nanorc \
-    ~/.netrc \
-    ~/.ssh \
-    ~/.z   \
-        ~/migration/home
-
-cp -Rp ~/Documents ~/migration
-
-cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration/rootLibrary/Preferences/SystemConfiguration/ # wifi
-
-cp -Rp ~/Library/Preferences/net.limechat.LimeChat.plist ~/migration/Library/Preferences/
-cp -Rp ~/Library/Preferences/com.tinyspeck.slackmacgap.plist ~/migration/Library/Preferences/
-
-cp -Rp ~/Library/Services ~/migration/Library/ # automator stuff
-cp -Rp ~/Library/Fonts ~/migration/Library/ # all those fonts you've installed
-
-# editor settings & plugins
-cp -Rp ~/Library/Application\ Support/Sublime\ Text\ * ~/migration/Library/"Application Support"
-cp -Rp ~/Library/Application\ Support/Code\ -\ Insider* ~/migration/Library/"Application Support"
-
-# also consider...
-# random git branches you never pushed anywhere?
-# git untracked files (or local gitignored stuff). stuff you never added, but probably want..
-
-
-# OneTab history pages, because chrome tabs are valuable.
-
-# usage logs you've been keeping.
-
-# iTerm settings.
-  # Prefs, General, Use settings from Folder
-
-# Finder settings and TotalFinder settings
-#   Not sure how to do this yet. Really want to.
-
-# Timestats chrome extension stats
-#   chrome-extension://ejifodhjoeeenihgfpjijjmpomaphmah/options.html#_options
-# 	gotta export into JSON through devtools:
-#     copy(JSON.stringify(localStorage))
-#     pbpaste > timestats-canary.json.txt
-
-# software licenses.
-#   sublimetext's is in its Application Support folder
-
-# maybe ~/Pictures and such
-cp -Rp ~/Pictures ~/migration
-
-### end of old machine backup
-##############################################################################################################
-
-
-
-##############################################################################################################
-### XCode Command Line Tools
-#      thx https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
-
+# XCode Command Line Tools
+# https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
 if ! xcode-select --print-path &> /dev/null; then
+  # Prompt user to install the XCode Command Line Tools
+  xcode-select --install &> /dev/null
 
-    # Prompt user to install the XCode Command Line Tools
-    xcode-select --install &> /dev/null
+  # Wait until the XCode Command Line Tools are installed
+  until xcode-select --print-path &> /dev/null; do
+      sleep 5
+  done
+  print_result $? 'Install XCode Command Line Tools'
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Point the `xcode-select` developer directory to the appropriate directory in
+  # `Xcode.app`.  see https://github.com/alrra/dotfiles/issues/13
+  sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+  print_result $? 'Make "xcode-select" developer directory point to Xcode'
 
-    # Wait until the XCode Command Line Tools are installed
-    until xcode-select --print-path &> /dev/null; do
-        sleep 5
-    done
-
-    print_result $? 'Install XCode Command Line Tools'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Point the `xcode-select` developer directory to
-    # the appropriate directory from within `Xcode.app`
-    # https://github.com/alrra/dotfiles/issues/13
-
-    sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-    print_result $? 'Make "xcode-select" developer directory point to Xcode'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Automatically agree to the terms of the Xcode license
-    # https://github.com/alrra/dotfiles/issues/10
-
-    sudo xcodebuild -license accept &> /dev/null
-    print_result $? 'Agree with the XCode Command Line Tools licence'
-
+  # Automatically agree to the terms of the Xcode license
+  # https://github.com/alrra/dotfiles/issues/10
+  sudo xcodebuild -license accept &> /dev/null
+  print_result $? 'Agree with the XCode Command Line Tools licence'
 fi
-###
-##############################################################################################################
 
-
-
-##############################################################################################################
-### homebrew!
-
-# first, install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# install, update and check Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+print_result $? 'Installed Homebrew'
 brew update
+print_result $? 'Updated Homebrew'
 brew doctor
+print_result $? 'Homebrew checked by the doctor'
 
-# then, install all the things
-brew bundle --global
+# create the directory for Homebrew Casks and manually-installed third-party software
+mkdir ~/Applications
+print_result $? 'Created directory for Homebrew Casks'
 
-# lastly, remove outdated versions from the cellar
+# export some environment variables for Homebrew
+export HOMEBREW_CASK_OPTS="--appdir=~/Applications"  # keep Casks separate from the programs installed by macOS
+export HOMEBREW_EDITOR="${EDITOR}"  # use the system editor to edit Homebrew stuff
+export HOMEBREW_CACHE="/Volumes/ExternalHome/rtruell/HomebrewCache"  # get the cache off the SSD
+export BREW_PREFIX=$(brew --prefix)  # store Homebrew's installation directory so I don't have to keep issuing the command
+print_result 0 'Exported Homebrew environment variables'
+
+# install all the things
+echo "About to install the .Brewfile contents...this could take a while!!"
+brew bundle --global 2>&1 | tee ~/bundle-install.txt
+print_result $? 'Installed desired formula, Casks and MAS apps'
+
+# remove outdated versions from the cellar
 brew cleanup
-
-### end of homebrew
-##############################################################################################################
-
-
-
-
-##############################################################################################################
-### install of common things
-###
-
-# github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
-# autocompletion for git branch names https://git-scm.com/book/en/v1/Git-Basics-Tips-and-Tricks
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-
-
-# Type `git open` to open the GitHub page or website for a repository.
-npm install -g git-open
-
-# fancy listing of recent branches
-npm install -g git-recent
-
-# sexy git diffs
-npm install -g diff-so-fancy
-
-# trash as the safe `rm` alternative
-npm install --global trash-cli
-
-# more readable git diffs
-npm install --global diff-so-fancy
-
-# install better nanorc config
-# https://github.com/scopatz/nanorc
-curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
-
-# github.com/rupa/z   - oh how i love you
-git clone https://github.com/rupa/z.git ~/code/z
-# consider reusing your current .z file if possible. it's painful to rebuild :)
-# z is hooked up in .bash_profile
-
-
-# github.com/thebitguru/play-button-itunes-patch
-# disable itunes opening on media keys
-git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
-
-
-# my magic photobooth symlink -> dropbox. I love it.
-# 	 + first move Photo Booth folder out of Pictures
-# 	 + then start Photo Booth. It'll ask where to put the library.
-# 	 + put it in Dropbox/public
-# 	* Now… you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# 	* then you grab public URL and send off your video message in a heartbeat.
-
-
-# for the c alias (syntax highlighted cat)
-sudo easy_install Pygments
-
+print_result $? 'Cleaned up Homebrew'
 
 # switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells
-  chsh -s "${BREW_PREFIX}/bin/bash"
+if [[ -x "${BREW_PREFIX}/bin/bash" ]]; then
+  if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then  # if the new bash isn't already in the list of shell programs
+    echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells  # add it
+    print_result $? 'Updated "/etc/shell" with the just-installed version of bash'
+  fi
+  chsh -s "${BREW_PREFIX}/bin/bash"  # change the shell to the new bash
+  print_result $? 'Changed the shell to the new version of bash: $BASH_VERSION (should be 4+)'  # should be 4+ not the old 3.2.X
+else
+  print_result 1 'bash not installed properly by Homebrew'
 fi
-echo $BASH_VERSION # should be 4.x not the old 3.2.X
 
-# iterm with more margin! http://hackr.it/articles/prettier-gutter-in-iterm-2/
-#   (admittedly not as easy to maintain)
+# set up macOS defaults
+compname=$(hostname -s)  # get the hostname of the computer, stripping off the domainname if it's there
+source ./.macos-${compname}  # set up the defaults specific to the computer being installed
+print_result $? 'Set up the macOS defaults specific to this computer'
+source ./.macos  # set up the defaults common to all Mac computers
+print_result $? 'Set up the macOS defaults common to all Mac computers'
 
-
-# setting up the sublime symlink
-ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
-
-# install nvm (Node Version Nanager, https://github.com/nvm-sh/nvm)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
-
-###
-##############################################################################################################
-
-
-## Chromium hacking
-
-# improve perf of git inside of chromium checkout
-
-# read https://chromium.googlesource.com/chromium/src/+/master/docs/mac_build_instructions.md
-
-# default is (257*1024)
-sudo sysctl kern.maxvnodes=$((512*1024))
-echo kern.maxvnodes=$((512*1024)) | sudo tee -a /etc/sysctl.conf
-
-# https://facebook.github.io/watchman/docs/install.html#mac-os-file-descriptor-limits
-sudo sysctl -w kern.maxfiles=$((10*1024*1024))
-sudo sysctl -w kern.maxfilesperproc=$((1024*1024))
-echo kern.maxfiles=$((10*1024*1024)) | sudo tee -a /etc/sysctl.conf
-echo kern.maxfilesperproc=$((1024*1024)) | sudo tee -a /etc/sysctl.conf
-
-
-# speed up git status (to run only in chromium repo)
-git config status.showuntrackedfiles no
-git update-index --untracked-cache
-
-# faster git server communication.
-# like a LOT faster. https://opensource.googleblog.com/2018/05/introducing-git-protocol-version-2.html
-git config protocol.version 2
-
-# see also "A Chromium Compiling Setup for DevTools Hackers"
-# https://gist.github.com/paulirish/2d84a6db1b41b4020685
-
-# also this unrelated thing
-# git config user.email "xxxx@chromium.org"
-
-
-##############################################################################################################
-### remaining configuration
-###
-
-# go read mathias, paulmillr, gf3, alraa's dotfiles to see what's worth stealing.
-
-# prezto and antigen communties also have great stuff
-#   github.com/sorin-ionescu/prezto/blob/master/modules/utility/init.zsh
-
-# set up osx defaults
-#   maybe something else in here https://github.com/hjuutilainen/dotfiles/blob/master/bin/osx-user-defaults.sh
-source ./.macos-mini
-
-# setup and run Rescuetime!
-
-###
-##############################################################################################################
-
-
-
-##############################################################################################################
-### symlinks to link dotfiles into ~/
-###
-
-#   move git credentials into ~/.gitconfig.local    	http://stackoverflow.com/a/13615531/89484
-#   now .gitconfig can be shared across all machines and only the .local changes
-
-# symlink it up!
-source ./symlink.sh
-
-# add manual symlink for .ssh/config and probably .config/fish
-
-###
-##############################################################################################################
-
-# Ensure .ssh dir sanity
-#mkdir -p ~/.ssh
-#chmod 700 ~/.ssh
-#touch ~/.ssh/authorized_keys
-#chmod 600 ~/.ssh/authorized_keys
-
-# Local github files
-#touch ~/.gitconfig.local
-#chmod 600 ~/.gitconfig.local
+# extra commands to execute
+rmdir /Users/rtruell/Downloads  # remove the OS-installed 'Downloads' directory ...
+print_result $? 'Removed the OS-installed "Downloads" directory'
+symlink_single_file "/Volumes/Downloads/RecentDownloads" "/Users/rtruell/Downloads"  # ... and replace it with a symlink to the external downloads directory, to get it off the SSD
+symlink_single_file "/Volumes/ExternalHome/rtruell/SourceCode" "/Users/rtruell/SourceCode"  # make the directory of source code projects easy to get to
+if [[ -x /Users/rtruell/Applications/iTerm.app ]]; then
+  sudo mv /Applications/Utilities/Terminal.app /Applications/Utilities/Terminal-apple.app  # rename Apple's 'Terminal' program ...
+  print_result $? "Renamed Apple's terminal program"
+  sudo symlink_single_file "/Users/rtruell/Applications/iTerm.app" "/Applications/Utilities/Terminal.app"  # ... and replace it with a symlink to 'iTerm'
+fi
